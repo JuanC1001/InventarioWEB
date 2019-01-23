4a41af6c79149aa0428602d394501eb700077313
Luis Delgado <deliflowty@gmail.com>
2019-01-23 18:20:35 -0500

    Reestrucura

---
+++ AplicacionWEB/src/java/rnegocio/funciones/FPaginaRol.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rnegocio.funciones;
+
+import AccesoADatos.Comando;
+import AccesoADatos.Conexion;
+import AccesoADatos.Global;
+import AccesoADatos.Parametro;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import rnegocio.entidades.PaginaRol;
+
+/**
+ *
+ * @author MI PC
+ */
+public class FPaginaRol {
+    public static ArrayList<PaginaRol> PaginaRol_buscartodos() throws Exception {
+        //CREO LISTA QUE RECIBIRA LOS DATOS DEL RESULSET
+        ArrayList<PaginaRol> lista = new ArrayList<PaginaRol>();
+        PaginaRol obj = new PaginaRol();
+        ResultSet rs = null;
+        //LLAMO LA CONEXION
+        Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+        //DECLARO UN PREPAREDSTATEMENT QUE EJECUTARA LA SQL
+        PreparedStatement preStm = null;
+        try {
+            //declaro mi sql
+            String sql = "SELECT codigo, nombre FROM facturacion.PaginaRol;";
+            //creo mi preparedstatement
+            preStm = con.creaPreparedSmt(sql);
+            //ejecuto el prepardestatement y le asigno a mi resulset
+            rs = con.ejecutaPrepared(preStm);
+            obj = null;
+            while (rs.next()) {
+                obj = new PaginaRol();
+                obj.setCodigo(rs.getInt("codigo"));
+                //obj.setPagina(FPagina.pagina_buscartodos(rs.getInt("pcodigo_pagina")));
+                //obj.setRol(FPaginaRol.PaginaRol_buscartodos(rs.getInt("pcodigo_rol")));
+                
+                lista.add(obj);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            rs.close();
+            preStm.close();
+            con.desconectar();
+        }
+        return lista;
+
+    }
+
+    public static PaginaRol PaginaRol_buscarporid(int piPaginaRolid) throws Exception {
+        //CREO LISTA QUE RECIBIRA LOS DATOS DEL RESULSET
+        PaginaRol obj = new PaginaRol();
+        ResultSet rs = null;
+        //LLAMO LA CONEXION
+        Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+        //DECLARO UN PREPAREDSTATEMENT QUE EJECUTARA LA SQL
+        PreparedStatement preStm = null;
+        try {
+            //declaro mi sql
+            String sql = "SELECT *from usuarios.rol_pagina_buscar(?);";
+            //creo mi preparedstatement
+            preStm = con.creaPreparedSmt(sql);
+            //ejecuto el prepardestatement y le asigno a mi resulset
+            preStm.setInt(1, piPaginaRolid);
+            rs = con.ejecutaPrepared(preStm);
+            obj = null;
+            while (rs.next()) {
+                obj = new PaginaRol();
+                obj.setCodigo(rs.getInt("pcodigo"));
+                obj.setPagina(FPagina.pagina_buscarporid(rs.getInt("pcodigo_pagina")));
+                obj.setRol(FRol.rol_buscarporid(rs.getInt("pcodigo_rol")));
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            rs.close();
+            preStm.close();
+            con.desconectar();
+        }
+        return obj;
+
+    }
+
+    public static boolean PaginaRol_insertar(PaginaRol PaginaRol) throws Exception {
+        boolean respuesta = false;
+        Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+        try {
+            //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+            ArrayList<Comando> comandos = new ArrayList<Comando>();
+            //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+            Comando cmd = new Comando();
+            //SETEAMOS LA FUNCION AL COMAND0
+            cmd.setSetenciaSql("SELECT *FROM usuarios.rol_pagina_insertar(?,?,?);");
+            //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+            ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+            //llenamos el arraylist con todos los parametros
+            parametros.add(new Parametro(1, PaginaRol.getCodigo()));
+
+            //llenar el comando con los parametros
+            cmd.setLstParametros(parametros);
+            comandos.add(cmd);
+            //llamamos al funcion que ejecuta la transaccion en la base de datos
+            respuesta = con.ejecutaPreparedTransaccion(comandos);
+
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return respuesta;
+
+    }
+
+    public static boolean PaginaRol_editar(PaginaRol PaginaRol) throws Exception {
+        boolean respuesta = false;
+        Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+        try {
+            //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+            ArrayList<Comando> comandos = new ArrayList<Comando>();
+            //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+            Comando cmd = new Comando();
+            //SETEAMOS LA FUNCION AL COMAND0
+            cmd.setSetenciaSql("SELECT usuarios.rol_pagina_editar(?,?,?);");
+            //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+            ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+            //llenamos el arraylist con todos los parametros
+
+            parametros.add(new Parametro(1, PaginaRol.getCodigo()));
+            parametros.add(new Parametro(2, PaginaRol.getPagina()));
+             parametros.add(new Parametro(3, PaginaRol.getRol()));
+            
+
+            //llenar el comando con los parametros
+            cmd.setLstParametros(parametros);
+            comandos.add(cmd);
+            //llamamos al funcion que ejecuta la transaccion en la base de datos
+            respuesta = con.ejecutaPreparedTransaccion(comandos);
+
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return respuesta;
+
+    }
+
+    public static boolean PaginaRol_eliminar(int pscactbevidenid) throws Exception {
+        boolean respuesta = false;
+        Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+        try {
+            //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+            ArrayList<Comando> comandos = new ArrayList<Comando>();
+            //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+            Comando cmd = new Comando();
+            //SETEAMOS LA FUNCION AL COMAND0
+            cmd.setSetenciaSql("SELECT usuarios.rol_pagina_eliminar(?);");
+            //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+            ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+            //llenamos el arraylist con todos los parametros
+            parametros.add(new Parametro(1, pscactbevidenid));
+            //llenar el comando con los parametros
+            cmd.setLstParametros(parametros);
+            comandos.add(cmd);
+            //llamamos al funcion que ejecuta la transaccion en la base de datos
+            respuesta = con.ejecutaPreparedTransaccion(comandos);
+
+        } catch (SQLException e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            con.desconectar();
+        }
+        return respuesta;
+
+    }
+  
+}
