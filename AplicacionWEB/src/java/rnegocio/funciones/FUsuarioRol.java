75d9fa18a9931a228891ffb39f414134541d0aa5
Usuario <Usuario@FI-LABC05-PC17>
2019-01-23 16:37:16 -0500

    entidad UsuarioRol

---
+++ AplicacionWEB/src/java/rnegocio/funciones/FUsuarioRol.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rnegocio.funciones;
+
+import rnegocio.entidades.Rol;
+import rnegocio.entidades.Usuario;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FUsuarioRol {
+
+}
