c1abd00a35b56ca56af6a07f6f69a388f53da6cc
Usuario <Usuario@FI-LABC05-PC17>
2019-01-23 15:10:07 -0500

    entidad UsuarioRol

--- AplicacionWEB/src/java/rnegocio/entidades/UsuarioRol.java
+++ AplicacionWEB/src/java/rnegocio/entidades/UsuarioRol.java
@@ -10,5 +10,52 @@ package rnegocio.entidades;
  * @author Usuario
  */
 public class UsuarioRol {
-    
+
+    private int codigo;
+    private Rol rol;
+    private Usuario usuario;
+    private int estado;
+
+    public UsuarioRol() {
+    }
+
+    public UsuarioRol(int codigo, Rol rol, Usuario usuario, int estado) {
+        this.codigo = codigo;
+        this.rol = rol;
+        this.usuario = usuario;
+        this.estado = estado;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
 }
